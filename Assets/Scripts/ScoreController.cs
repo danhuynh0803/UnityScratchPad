0827f11ec23bbd2f3ae1175a1d5fc81cc04faabc
Danny Huynh <danhuynh0803@gmail.com>
2015-12-05 22:25:23 -0500

    Added a band-aid image that appears after player loses 50% of health. Created a score text in HUD to keep track of player's score.

---
+++ Assets/Scripts/ScoreController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreController : MonoBehaviour {
+
+	public GUIText scoreTotal; 
+	private int score = 0; 
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		scoreTotal.text = "Score: " + score; 
+	}
+	
+	void addScore(int score)
+	{
+		this.score += score; 
+	}
+}
